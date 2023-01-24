2b5dda6e0874a454ecfddd58754890c7f578ad8d
Grimmjow2904 <akito2904@gmail.com>
2023-01-24 04:14:19 -0500

    pages

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-yarn build
+npm run build
 
 # navigate into the build output directory
 cd dist
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:Grimmjo2904/Grimmjo2904.github.io.git main
+ git push -f git@github.com:grimmjow2904/grimmjow2904.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,27 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from "vitest";
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import { mount } from "@vue/test-utils";
+import RepoCard from "../RepoCard.vue";
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+describe("HelloWorld", () => {
+  it("renders properly", () => {
+    const wrapper = mount(RepoCard, {
+      props: {
+        repo: {
+          repoName: "portfolio",
+          repoDesc: null,
+          repoUrl: "https://github.com/Grimmjow2904/portfolio",
+          repoLang: {
+            Vue: 5099,
+            TypeScript: 3587,
+            JavaScript: 674,
+            Shell: 581,
+            HTML: 331,
+            CSS: 298,
+          },
+        },
+      },
+    } as any);
+    expect(wrapper.text()).toContain("Hello Vitest");
+  });
+});
